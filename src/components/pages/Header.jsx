0eb14ecc3aef646889a230a92c1d3b7fa12b7e5e
Wellington <tinhoefaela@gmail.com>
2022-01-13 19:37:15 -0300

    task 4

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
-export default class Header extends Component {
+class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      src: '',
+    };
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI = async () => {
+    const { email } = this.props;
+    const hash = md5(email).toString();
+    console.log(email);
+    console.log(hash);
+    const curr = `https://www.gravatar.com/avatar/${hash}`;
+    const response = await fetch(curr);
+    const json = await response;
+    const src = json.url;
+    this.setState({ src });
+  }
+
   render() {
+    const { name } = this.props;
+    const { src } = this.state;
     return (
-      <header className="App-header">
-        {/* <img src={ logo } className="App-logo" alt="logo" weight="10rm" /> */}
-      </header>
+      <div>
+        <img
+          src={ src }
+          alt={ src }
+          data-testid="header-profile-picture"
+        />
+        <h3 data-testid="header-player-name">{name}</h3>
+        <h2 data-testid="header-score">0</h2>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  email: state.player.email,
+  name: state.player.name,
+});
+
+export default connect(mapStateToProps)(Header);
