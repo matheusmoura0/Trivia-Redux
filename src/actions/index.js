238f7121e318eaa513bc2ef70ff95515876ea019
Danilo Couto <couto.danilo95@gmail.com>
2022-01-14 16:33:29 -0300

    grupo-8 feat: requisito 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
 export const SET_EMAIL_ACTION = 'SET_EMAIL';
 export const TOKEN_ACTION = 'TOKEN_ACTION';
 
+export const playerAction = (payload) => ({
+  type: SET_EMAIL_ACTION,
+  payload,
+});
+
 export const tokenAction = (payload) => ({
   type: TOKEN_ACTION,
   payload,
