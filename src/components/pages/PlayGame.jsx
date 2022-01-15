c6b061e10baf76e9530fe8c96728917a8050d635
Matheus Moura <matheusmoura2110@gmail.com>
2022-01-14 23:39:54 -0300

    fixing task 5

--- src/components/pages/PlayGame.jsx
+++ src/components/pages/PlayGame.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
 
 class PlayGame extends Component {
@@ -26,7 +27,8 @@ class PlayGame extends Component {
     this.setState({
       question: questionsObjt.results[0],
       played: true,
-      arraySortedAlternatives: [questionsObjt.results[0].incorrect_answers, questionsObjt.results[0].correct_answer],
+      arraySortedAlternatives: [...questionsObjt.results[0].incorrect_answers,
+        questionsObjt.results[0].correct_answer],
     });
   };
 
@@ -38,38 +40,48 @@ class PlayGame extends Component {
       <div>
         <Header />
         {!played ? null
-          : <div>
-            <p
-              data-testid="question-category"
-            >
-              {question.category}
-            </p>
-            <p
-              data-testid="question-text"
-            >
-              {question.question}
-            </p>
-            <p
-              data-testid="answer-options"
-            >
-              {arraySortedAlternatives.sort(() => Math.random() - magicNumber).map((el, i) => (
-                <button type="button" key={ i } data-testid={ `wrong-answer-${i}` }>{el}</button>
-              ))}
-              {/* <button type="button" data-testid="correct-answer">{question.correct_answer}</button> */}
-            </p>
-            </div>}
+          : (
+            <div>
+              <p
+                data-testid="question-category"
+              >
+                {question.category}
+              </p>
+              <p
+                data-testid="question-text"
+              >
+                {question.question}
+              </p>
+              <p
+                data-testid="answer-options"
+              >
+                {arraySortedAlternatives
+                  .sort(() => Math
+                    .random() - magicNumber)
+                  .map((el, i) => (
+                    <button
+                      type="button"
+                      key={ i }
+                      data-testid={ `wrong-answer-${i}` }
+                    >
+                      {el}
+
+                    </button>
+                  ))}
+              </p>
+            </div>)}
       </div>
     );
   }
 }
 
-/*
-As alternativas devem ser exibidas em ordem aleatória
-Dica: utilize botões (<button/>) para as alternativas
- */
 const mapStateToProps = (state) => ({
   token: state.token,
   questions: state.player.questions,
 });
+
+PlayGame.propTypes = {
+  token: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps)(PlayGame);
